cf11831feeb3c7b0ea1bc0a786ff4de0e62d0cf4
izumi tetsuya <t.izumi@andco-web.jp>
2025-01-04 12:51:33 +0900

    upda te

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -58,10 +58,9 @@ export async function createInvoice(prevState: State, formData: FormData) {
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
     } catch (error) {
-    // If a database error occurs, return a more specific error.
-    return {
-        message: 'Database Error: Failed to Create Invoice.',
-    };
+        return {
+            message: 'Database Error: Failed to Create Invoice.',
+        };
     }
 
     // Revalidate the cache for the invoices page and redirect the user.
