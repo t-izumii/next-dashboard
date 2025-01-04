4e6d4e2370460fcf455ad144c334c3fd02bcf480
izumi tetsuya <t.izumi@andco-web.jp>
2025-01-04 17:49:12 +0900

    up

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -106,13 +106,10 @@ export async function updateInvoice(
 
 
 export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
     try {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
         revalidatePath('/dashboard/invoices');
-        return { message: 'Deleted Invoice.' };
     } catch (error) {
-        return { message: 'Database Error: Failed to Delete Invoice.' };
     }
 }
 
